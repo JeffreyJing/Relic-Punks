48bad9ce3e09325ffda9a725d9278ae9327c7507
William Kwok <wkwok1998@gmail.com>
2022-04-03 12:08:04 -1000

    Added names to all punks

--- src/components/punks/index.js
+++ src/components/punks/index.js
@@ -1,7 +1,7 @@
 import p2140 from '../../assets/images/punks/2140.jpg';
 import p2140gv from '../../assets/images/punks/2140garryv.jpg';
 import p2338 from '../../assets/images/punks/2338.jpg';
-import p3609 from '../../assets/images/punks/3609.jpg';
+import p4609 from '../../assets/images/punks/4609.jpg';
 import p6578 from '../../assets/images/punks/6578.jpg';
 import p9671 from '../../assets/images/punks/9671.jpg';
 import patari from '../../assets/images/punks/atari.jpg';
@@ -40,138 +40,172 @@ import SimpleModal from 'simple-react-modal';
 
 const PUNK_ITEMS = [
     {
+        name: 'Cryptopunk #2140',
         image: p2140,
         id: 0
     },
     {
+        name: 'Cryptopunk #2140 - GarryVee',
         image: p2140gv,
         id: 1
     },
     {
+        name: 'Cryptopunk #2338 - 7CD3FC',
         image: p2338,
         id: 2
     },
     {
-        image: p3609,
+        name: 'Cryptopunk #4609',
+        image: p4609,
         id: 3
     },
     {
+        name: 'Cryptopunk #6578',
         image: p6578,
         id: 4
     },
     {
+        name: 'Cryptopunk #9671 - 0F70B8',
         image: p9671,
         id: 5
     },
     {
+        name: 'Atari Punk',
         image: patari,
         id: 6
     },
     {
+        name: 'ATM Punk',
         image: patm,
         id: 7
     },
     {
+        name: 'Crypto C3P0 Punk',
         image: pc3po,
         id: 8
     },
     {
+        name: 'Casio Punk',
         image: pcasio,
         id: 9
     },
     {
+        name: 'Cola Punk',
         image: pcola,
         id: 10
     },
     {
+        name: 'Easy Baked Punk',
         image: pezbaked,
         id: 11
     },
     {
+        name: 'Classic Football Punk',
         image: pfootball,
         id: 12
     },
     {
+        name: 'Gameboy Kevin Punk',
         image: pgameboykevin,
         id: 13
     },
     {
+        name: 'Gamecube Punk',
         image: pgamecube,
         id: 14
     },
     {
+        name: 'Hello Punk',
         image: phello,
         id: 15
     },
     {
+        name: 'Iron man Punk',
         image: pironman,
         id: 16
     },
     {
+        name: 'Juke Punk',
         image: pjuke,
         id: 17
     },
     {
+        name: 'Ledger Punk',
         image: pledger,
         id: 18
     },
     {
+        name: 'Lite Punk',
         image: plite,
         id: 19
     },
     {
+        name: 'Mac Punk',
         image: pmac,
         id: 20
     },
     {
+        name: 'Mag Punk',
         image: pmag,
         id: 21
     },
     {
+        name: 'MPC Punk',
         image: pmpc,
         id: 22
     },
     {
+        name: 'N64 Punk',
         image: pn64,
         id: 23
     },
     {
+        name: 'Pac Punk',
         image: ppac,
         id: 24
     },
     {
+        name: 'Phone Punk',
         image: pphone,
         id: 25
     },
     {
+        name: 'Polaroid Punk',
         image: ppolaroid,
         id: 26
     },
     {
+        name: 'Pop Punk',
         image: ppop,
         id: 27
     },
     {
+        name: 'Prime Punk',
         image: pprime,
         id: 28
     },
     {
+        name: 'R2D2 Punk',
         image: pr2d2,
         id: 29
     },
     {
+        name: 'Speakspell Punk',
         image: pspeakspell,
         id: 30
     },
     {
+        name: 'Tiffany Watch Punk',
         image: ptiffanywatch,
         id: 31
     },
     {
+        name: 'VW Punk',
         image: pvw,
         id: 32
     },
     {
+        name: 'Walkman Punk',
         image: pwalkman,
         id: 33
     },
@@ -205,7 +239,12 @@ const Punks = () => {
 
     return (
         <>
-            <SimpleModal show={activePunk !== undefined} onClose={() => setActivePunk(undefined)}>
+            <SimpleModal
+                show={activePunk !== undefined} onClose={() => setActivePunk(undefined)}
+                style={{
+                    backgroundColor: 'black'
+                }}
+            >
 
             </SimpleModal>
             <div className="punks">
