b58ccca90c3c75e60ca5d708da4a3b26c8b89974
William Kwok <wkwok1998@gmail.com>
2022-04-03 11:38:59 -1000

    Added juseffect

--- src/components/relics-pass-status/index.js
+++ src/components/relics-pass-status/index.js
@@ -1,13 +1,26 @@
 import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import { useContext, useEffect } from 'react/cjs/react.production.min';
+import { Web3Context } from '../../context/web3-context';
 
 import './index.css';
 
 const RelicsPassStatus = () => {
+    const { connected } = useContext(Web3Context);
     const [hasPass, setHasPass] = useState(false);
 
-    console.log("here")
+    useEffect(async () => {
+        let canceled = false;
+
+        if (connected) {
+            // TODO: READ CONTRACT HERE AND SET HAS PASS BASED ON THAT
+        }
+
+        return () => {
+            canceled = true;
+        };
+    }, [connected])
 
     return (
         <p>Relics Pass <span className={
