e70d24f24810978cfa92f575b116e1f91b1cef3e
William Kwok <mockingod@gmail.com>
2022-04-06 09:35:32 -0700

    added transition

--- src/components/punks/index.js
+++ src/components/punks/index.js
@@ -28,10 +28,10 @@ import pvw from '../../assets/images/punks/vw.jpg';
 import pwalkman from '../../assets/images/punks/walkman.jpg';
 import pjuke from '../../assets/images/punks/juke.jpg';
 
-import { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import './index.css';
-import SimpleModal from 'simple-react-modal';
+// import SimpleModal from 'simple-react-modal';
 
 import { Web3Context } from '../../context/web3-context';
 
@@ -232,6 +232,7 @@ const Punks = () => {
                     width: width < 1000 ? 300 : 500,
                     padding: width < 1000 ? 10 : 20,
                 }}
+                transitionSpeed={250}
             >
                 {activePunkItem && (
                     <div className='active-punk'>
@@ -261,5 +262,144 @@ const Punks = () => {
         </>
     )
 }
+
+var modal = {
+    position: 'fixed',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    background: 'rgba(0, 0, 0, 0.8)',
+    zIndex: 99999,
+    transition: 'opacity 250ms ease-in',
+    pointerEvents: 'auto',
+    overflowY: 'auto'
+  }
+  
+  var container = {
+    width: '400px',
+    position: 'relative',
+    margin: '10% auto',
+    padding: '5px 20px 13px 20px',
+    background: '#fff'
+  }
+  
+  var close = {
+    background: '#606061',
+    color: '#FFFFFF',
+    lineHeight: '25px',
+    position: 'absolute',
+    right: '-12px',
+    textAlign: 'center',
+    top: '-10px',
+    width: '24px',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    borderRadius: '12px',
+    boxShadow: '1px 1px 3px #000',
+    cursor: 'pointer'
+  }
+
+class SimpleModal extends React.Component{
+
+    constructor(props){
+      super()
+      this.hideOnOuterClick = this.hideOnOuterClick.bind(this)
+      this.fadeIn = this.fadeIn.bind(this)
+      this.fadeOut = this.fadeOut.bind(this)
+  
+      let opacity = 0,
+        display = 'block',
+        visibility = 'hidden'
+  
+      if(props.show){
+        opacity = 1
+        display = 'block'
+        visibility = 'visible'
+      }
+  
+      this.state = {
+        opacity,
+        display,
+        visibility,
+        show: props.show
+      }
+  
+    }
+  
+    hideOnOuterClick(event){
+      if(this.props.closeOnOuterClick === false) return
+      if(event.target.dataset.modal && this.props.onClose instanceof Function) this.props.onClose(event)
+    }
+  
+    componentWillReceiveProps(props){
+      if(this.props.show != props.show){
+        if(this.props.transitionSpeed){
+          if(props.show == true) this.fadeIn()
+          else this.fadeOut()
+        }
+        else this.setState({show: props.show})
+      }
+    }
+  
+    fadeIn(){
+      this.setState({
+        display: 'block',
+        visibility: 'visible',
+        show: true
+      }, ()=>{
+        setTimeout(()=>{
+          this.setState({opacity: 1})
+        },10)
+      })
+    }
+  
+    fadeOut(){
+      this.setState({opacity: 0}, ()=>{
+        setTimeout(()=>{
+          this.setState({show: false})
+        }, this.props.transitionSpeed)
+      })
+    }
+  
+    render(){
+      if(!this.state.show) return null
+      let modalStyle, containerStyle
+      //completely overwrite if they use a class
+      if(this.props.className){
+        modalStyle = this.props.style
+        containerStyle = this.props.containerStyle
+      }
+      else{
+        modalStyle = Object.assign({}, modal, this.props.style)
+        containerStyle = Object.assign({}, container, this.props.containerStyle)
+      }
+      if(this.props.transitionSpeed) modalStyle = Object.assign({}, this.state, modalStyle)
+  
+      return (
+        <div {..._filteredProps(this.props)} style={modalStyle} onClick={this.hideOnOuterClick} data-modal="true">
+          <div className={this.props.containerClassName} style={containerStyle}>
+            {this.props.children}
+          </div>
+        </div>
+      )
+    }
+  }
+  
+  function _filteredProps(props) {
+    const filtered = Object.assign({}, props);
+    [
+      'containerStyle',
+      'containerClassName',
+      'closeOnOuterClick',
+      'show',
+      'onClose',
+      'transitionSpeed'
+    ].map( p => {
+      delete filtered[p]
+    })
+    return filtered
+  }
 
 export default Punks;
