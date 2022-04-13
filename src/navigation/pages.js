6c505f51170b1f6babdfafb42342989039b77264
JeffreyJing <jjingdn@gmail.com>
2022-04-13 16:12:29 -0700

    no nav

--- src/navigation/pages.js
+++ src/navigation/pages.js
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from '../components/navbar';
 import ClaimableSearch from '../pages/claimable-search';
 import Home from '../pages/home';
 
 const Pages = () => {
 	return (
 		<>
-			<Navbar />
 			<Routes>
 				<Route index path='/' element={<Home />} />
 				<Route index path='claimable-search' element={<ClaimableSearch />} />
